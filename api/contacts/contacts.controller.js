ed03f4790d6b0658780cb093e4882dd6e8ba0463
Yelyzaveta Domasheva <lizidomasheva@gmail.com>
2020-07-11 10:03:25 +0300

    hw-auth-v1

--- api/contacts/contacts.controller.js
+++ api/contacts/contacts.controller.js
@@ -1,13 +1,33 @@
 const Joi = require('@hapi/joi');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const contactModel = require('./contacts.model');
 const {
   Types: { ObjectId },
 } = require('mongoose');
+const { UnauthorizedError } = require('../helpers/errors.constructors');
 
 class ContactController {
+  constructor() {
+    this._costFactor = 7;
+  }
+
+  get getContacts() {
+    return this._getContacts.bind(this);
+  }
+
   get getContactById() {
     return this._getContactById.bind(this);
   }
+
+  get getCurrentContact() {
+    return this._getCurrentContact.bind(this);
+  }
+
+  get getContactsWithQuery() {
+    return this._getContactsWithQuery.bind(this);
+  }
+
   get createContact() {
     return this._createContact.bind(this);
   }
@@ -16,13 +36,30 @@ class ContactController {
     return this._updateContact.bind(this);
   }
 
+  get updateSubscription() {
+    return this._updateSubscription.bind(this);
+  }
+
   get deleteContact() {
     return this._deleteContact.bind(this);
   }
 
-  async getContacts(req, res, next) {
+  async _getContacts(req, res, next) {
     const contacts = await contactModel.find();
-    return res.status(200).json(contacts);
+    return res.status(200).json(this.prepareContactsResponse(contacts));
+  }
+
+  async _getContactsWithQuery(req, res, next) {
+    if (req.query.sub) {
+      const contacts = await contactModel.find({ subscription: req.query.sub });
+      return res.status(200).json(this.prepareContactsResponse(contacts));
+    }
+    const options = {
+      page: req.query.page,
+      limit: req.query.limit,
+    };
+    const contacts = await contactModel.paginate({}, options);
+    return res.status(200).json(this.prepareContactsResponse(contacts.docs));
   }
 
   async _getContactById(req, res, next) {
@@ -32,31 +69,77 @@ class ContactController {
     if (!contact) {
       return res.status(404).json({ message: 'Not found' });
     }
-    return res.status(200).json(contact);
+
+    const [contactForResponse] = this.prepareContactsResponse([contact]);
+    return res.status(200).json(contactForResponse);
+  }
+
+  async _getCurrentContact(req, res, next) {
+    const [contactForResponse] = this.prepareContactsResponse([req.contact]);
+    return res.status(200).json(contactForResponse);
   }
 
   async _createContact(req, res, next) {
-    const contact = await contactModel.create(req.body);
-    return res.status(201).json(contact);
+    const { email, password, subscription, token } = req.body;
+    const passwordHash = await bcryptjs.hash(password, this._costFactor);
+    const existingContact = await contactModel.findContactByEmail(email);
+    if (existingContact) {
+      return res.status(409).json({ message: 'Email in use' });
+    }
+
+    const contact = await contactModel.create({
+      email,
+      subscription,
+      password: passwordHash,
+    });
+    return res.status(201).json({
+      contact: {
+        email: contact.email,
+        subscription: contact.subscription,
+      },
+    });
   }
 
   validateCreateContact(req, res, next) {
     const createContactRules = Joi.object({
-      name: Joi.string().required(),
       email: Joi.string().required(),
-      phone: Joi.string().required(),
-      subscription: Joi.string().required(),
       password: Joi.string().required(),
+      subscription: Joi.string(),
       token: Joi.string().allow('').allow(null),
     });
 
     const result = createContactRules.validate(req.body);
     if (result.error) {
-      return res.status(400).json({
-        message: 'Missing required name field',
-      });
+      return res.status(400).send(result.error);
     }
     next();
+  }
+
+  async authorize(req, res, next) {
+    try {
+      const authorizationHeader = req.get('Authorization') || '';
+      const token = authorizationHeader.replace('Bearer ', '');
+
+      let contactId;
+      try {
+        contactId = await jwt.verify(token, process.env.JWT_SECRET).id;
+      } catch (err) {
+        return res.status(401).json({ message: 'Not authorized' });
+      }
+
+      const contact = await contactModel.findById(contactId);
+
+      if (!contact || contact.token !== token) {
+        throw new UnauthorizedError();
+      }
+
+      req.contact = contact;
+      req.token = token;
+
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: 'Not authorized' });
+    }
   }
 
   validateId(req, res, next) {
@@ -64,7 +147,6 @@ class ContactController {
     if (!ObjectId.isValid(contactId)) {
       return res.status(400).json({ message: 'Not valid id' });
     }
-
     next();
   }
 
@@ -77,7 +159,11 @@ class ContactController {
     if (!updateContact) {
       return res.status(404).json({ massage: 'Not found' });
     }
-    return res.status(200).send(updateContact);
+
+    const [updateContactForResponse] = this.prepareContactsResponse([
+      updateContact,
+    ]);
+    return res.status(200).json(updateContactForResponse);
   }
 
   validateUpdateContact(req, res, next) {
@@ -105,6 +191,37 @@ class ContactController {
     next();
   }
 
+  async _updateSubscription(req, res, next) {
+    const contactId = req.params.contactId;
+    const updateContact = await contactModel.findContactByIdAndUpdate(
+      contactId,
+      req.body,
+    );
+    if (!updateContact) {
+      return res.status(404).json({ massage: 'Not found' });
+    }
+
+    const [updateContactForResponse] = this.prepareContactsResponse([
+      updateContact,
+    ]);
+    return res.status(200).json(updateContactForResponse);
+  }
+
+  validateUpdateSubscription(req, res, next) {
+    const values = ['free', 'pro', 'premium'];
+    const updateSubRules = Joi.object({
+      subscription: Joi.string().valid(...values),
+    });
+
+    const result = updateSubRules.validate(req.body);
+    if (result.error) {
+      return res.status(400).send({
+        message: 'not valid value',
+      });
+    }
+    next();
+  }
+
   async _deleteContact(req, res, next) {
     const contactId = req.params.contactId;
     const deleteContact = await contactModel.findByIdAndDelete(contactId);
@@ -114,6 +231,60 @@ class ContactController {
     }
     return res.status(200).json({
       message: 'contact deleted',
+    });
+  }
+
+  async login(req, res, next) {
+    const { email, password, subscription } = req.body;
+
+    const contact = await contactModel.findContactByEmail(email);
+
+    if (!contact) {
+      return res.status(401).send('Email or password is wrong');
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, contact.password);
+    if (!isPasswordValid) {
+      return res.status(401).send('Email or password is wrong');
+    }
+
+    const token = await jwt.sign({ id: contact._id }, process.env.JWT_SECRET, {
+      expiresIn: 2 * 24 * 60 * 60,
+    });
+    await contactModel.updateToken(contact._id, token);
+
+    return res.status(200).json({
+      token,
+      contact: {
+        email: contact.email,
+        subscription: contact.subscription,
+      },
+    });
+  }
+
+  validateLogin(req, res, next) {
+    const loginRules = Joi.object({
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    });
+
+    const result = loginRules.validate(req.body);
+    if (result.error) {
+      return res.status(400).send(result.error);
+    }
+    next();
+  }
+
+  async logout(req, res, next) {
+    const contact = req.contact;
+    await contactModel.updateToken(contact._id, null);
+    return res.status(204).send('No Content');
+  }
+
+  prepareContactsResponse(contacts) {
+    return contacts.map(contact => {
+      const { _id, name, email, phone, subscription } = contact;
+      return { id: _id, name, email, phone, subscription };
     });
   }
 }
